d70a052038ec35f3b3d3f47dd2844859e3870bdf
John Running <john@joincake.com>
2023-12-06 14:00:31 -0500

    more updates for geo

--- app/api/geo/route.ts
+++ app/api/geo/route.ts
@@ -1,5 +1,24 @@
-import { NextRequest, NextResponse } from "next/server";
+//app/api/edge-geo/route.ts
+import { geolocation, ipAddress } from "@vercel/edge";
 
-export const GET = (request: NextRequest) => {
-  return NextResponse.json(request.geo);
-};
+export const runtime = "edge";
+
+export function GET(request: Request) {
+  const { city, country, latitude, longitude, countryRegion, flag } =
+    geolocation(request);
+  const ip = ipAddress(request) || "unknown";
+  return new Response(
+    JSON.stringify({
+      city,
+      countryRegion,
+      country,
+      flag,
+      latitude,
+      longitude,
+      ip,
+    }),
+    {
+      headers: { "content-type": "application/json" },
+    }
+  );
+}
